95edde0e1e6ef2153086d77058ec83223a27c975
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-07-23 18:27:51 +0530

    feat: initialise pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginUserPage from './pages/LoginUserPage'
+import RegisterUserPage from './pages/RegisterUserPage'
+import UserProfilePage from './pages/UserProfile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginUserPage />} />
+      <Route path='/register' element={<RegisterUserPage />} />
+      <Route path='/profile' element={<UserProfilePage />} />
+    </Routes>
   )
 }
 
