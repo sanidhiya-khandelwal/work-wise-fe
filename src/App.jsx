9ec86bd32d0bedc62b7e2805d80efe0c25b2e8c5
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-07-25 10:20:27 +0530

    feat : layout implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import HomePage from './pages/HomePage'
 import LoginUserPage from './pages/LoginUserPage'
 import RegisterUserPage from './pages/RegisterUserPage'
 import UserProfilePage from './pages/UserProfile'
+import MainLayout from './layouts/MainLayout'
+import NotFound404 from './pages/NotFound404'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/login' element={<LoginUserPage />} />
-      <Route path='/register' element={<RegisterUserPage />} />
-      <Route path='/profile' element={<UserProfilePage />} />
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginUserPage />} />
+        <Route path='/register' element={<RegisterUserPage />} />
+        <Route path='/profile' element={<UserProfilePage />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Route>
     </Routes>
   )
 }
