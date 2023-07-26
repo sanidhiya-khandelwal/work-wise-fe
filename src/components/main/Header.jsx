8d39517ede37398b8c46c4701e3cb105d8ae5f5f
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-07-26 12:04:59 +0530

    feat: app header

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Logo from '../../assets/logo-transparent.png'
+import '../../App.css'
+import { Link } from 'react-router-dom'
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="header-container">
+                <div className="header-left">
+                    <Link to='/' className="header-brand">
+                        <img src={Logo} alt="" />
+                        <h2>Work Wise</h2>
+                    </Link>
+                </div>
+                <div className="header-right">
+                    <div className="header-login">
+                        <Link to='/login'>Login</Link> {/**styling link in index.css with anchor tag */}
+                        <Link to='/register'>Register</Link>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
