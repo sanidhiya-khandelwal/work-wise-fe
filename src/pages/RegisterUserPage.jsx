1dbcc4ed53d67eda5f8abd8625b8df7d3eb16c33
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-07-27 13:31:14 +0530

    feat: register form ui

--- src/pages/RegisterUserPage.jsx
+++ src/pages/RegisterUserPage.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const RegisterUserPage = () => {
     return (
-        <div>RegisterUserPage</div>
+        <div className="register">
+            <div className="register-container">
+                <div className="register-form">
+                    <h1>Sign Up</h1>
+                    <form action="">
+                        <input type="text" className='input-field' placeholder='Full Name' required />
+                        <input type="number" className='input-field' placeholder='Phone No.' required />
+                        <input type="email" className='input-field' placeholder='Email' required />
+                        <input type="text" className='input-field' placeholder='User Name' required />
+                        <input type="password" className='input-field' placeholder='Password' required />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
